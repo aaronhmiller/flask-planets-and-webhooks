e337931e62e14f331b238d4af701fa1faed45b7a
Aaron Miller <aaron@konghq.com>
2021-04-24 19:49:02 -0700

    added stop & restart

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ else
   docker build -t kongaaron/flask-planets:$1 -t kongaaron/flask-planets:latest .
   docker push kongaaron/flask-planets&& docker push kongaaron/flask-planets:$1
 fi
+docker stop planets && docker rm planets
+docker run -d --network docker-compose_default --name planets -p 5000:5000 kongaaron/flask-planets
